98dd60dc4dba5359f144395ed228124e67dd2eb0
Aloisius92 <luigiolivella@gmail.com>
2016-05-24 18:08:40 +0200

    Fix

--- src/robocup/RoboCup.java
+++ src/robocup/RoboCup.java
@@ -4,15 +4,11 @@ import org.apache.log4j.BasicConfigurator;
 
 import com.github.robocup_atan.atan.model.AbstractTeam;
 
-import robocup.formation.FormationManager;
-
 public class RoboCup {
 
     public static void main(String[] args) {
         BasicConfigurator.configure();
-        
-        FormationManager.getInstance();
-        
+                
         AbstractTeam team = new Team("DropTableUsers");
         team.connectAll();
         
