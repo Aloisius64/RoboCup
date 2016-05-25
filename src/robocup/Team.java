769b40750c61db970e789ca0dc4d3714d9dbab73
Aloisius92 <luigiolivella@gmail.com>
2016-05-25 16:36:10 +0200

    Added support for more formation. Added actions for GOAP.

--- src/robocup/Team.java
+++ src/robocup/Team.java
@@ -10,14 +10,23 @@ import robocup.utility.Settings;
 
 public class Team extends AbstractTeam {
 
+	private String formation = null;	// Team formation name
+
 	public Team(String teamName) {
 		super(teamName, Settings.PORT_NUMBER, Settings.IP_SERVER, false);
+	}
+	
+	public Team(String teamName, String formation) {
+		super(teamName, Settings.PORT_NUMBER, Settings.IP_SERVER, false);
+		this.formation  = formation;
 	}
 
 	@Override
 	public ControllerPlayer getNewControllerPlayer(int i) {
+		if(formation!=null){
+			return FormationManager.getFormation(formation).getPlayer(i);
+		}
 		return new AttackerPlayer();
-		//return FormationManager.getFormation().getPlayer(i);
 	}
 
 	@Override
---
+++ src/robocup/player/actions/PassAction.java
@@ -0,0 +1,31 @@
+package robocup.player.actions;
+
+import robocup.ai.GoapAction;
+
+public class PassAction extends GoapAction {
+
+	@Override
+	public void reset() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean isDone() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean checkProceduralPrecondition(Object agent) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean perform(Object agent) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
